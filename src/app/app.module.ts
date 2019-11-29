11483198c29a50ca0a9cfa8966d528c04367411b
simplon-romainB <neriaa@hotmail.fr>
2019-11-29 12:29:54 +0100

    effetcs

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { BlogComponent } from './blog/blog.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProjectsComponent,
+    BlogComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
