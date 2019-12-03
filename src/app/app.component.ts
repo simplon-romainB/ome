84ea3148f49af119bd25146eb7c62d645270811b
simplon-romainB <neriaa@hotmail.fr>
2019-12-03 12:30:51 +0100

    animations

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import {RouterOutlet} from '@angular/router';
+import { slideInAnimation } from './route-animations';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.styl']
+  styleUrls: ['./app.component.styl'],
+  animations: [ slideInAnimation ]
 })
 export class AppComponent {
   title = 'ome';
+
+  prepareRoute(outlet: RouterOutlet) {
+    return outlet && outlet.activatedRouteData && outlet.activatedRouteData['animation'];
+  }
+
+  
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { RouterModule } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -16,7 +20,15 @@ import { HomeComponent } from './home/home.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgbModule,
+    FontAwesomeModule,
+    BrowserAnimationsModule,
+    RouterModule.forRoot([
+      {path:"blog", component: BlogComponent, data: {animation : "blogpage"} },
+      {path: "home", component: HomeComponent, data: {animation : "homepage"}},
+      {path: "projects", component: ProjectsComponent, data: {animation: "projectspage"}}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
