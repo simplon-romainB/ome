00c9c2aa621c29a4d15536893ea4fb100d875cd0
simplon-romainB <neriaa@hotmail.fr>
2020-01-29 17:11:09 +0100

    debuts tests unitaires

--- src/app/users.service.spec.ts
+++ src/app/users.service.spec.ts
@@ -1,12 +1,45 @@
-import { TestBed } from '@angular/core/testing';
+
+import { TestBed, getTestBed  } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { HttpClientModule, HttpClient} from '@angular/common/http'
 
 import { UsersService } from './users.service';
 
-describe('UsersService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
 
-  it('should be created', () => {
-    const service: UsersService = TestBed.get(UsersService);
-    expect(service).toBeTruthy();
+
+
+describe('UsersService', () => {
+  let injector: TestBed;
+  let service: UsersService;
+  let httpMock: HttpTestingController;
+
+  beforeEach(() => {TestBed.configureTestingModule({
+    imports: [HttpClientTestingModule],
+    providers: [UsersService]
+  })
+  
+  injector = getTestBed();
+  service = injector.get(UsersService);
+  httpMock = injector.get(HttpTestingController);
+});
+
+  it('should return articles', () => {
+    const dummyArticles = [
+      { title: 'article1', body: 'articlebody1', id: 1, image: 'articleimage1' },
+      { title: 'article2', body: 'articlebody2', id: 2, image: 'articleimage2' }
+    ];
+    service.initArticles().subscribe(articles => {
+      expect(Object.keys(articles).length).toBe(2);
+      expect(JSON.stringify(articles)).toEqual(JSON.stringify(dummyArticles));
+    });
+    const req = httpMock.expectOne("https://peaceful-mountain-88307.herokuapp.com/articles");
+    expect(req.request.method).toBe("GET");
+    req.flush(dummyArticles);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 });
+
+
