3191676a3acbba39df591eea9bd74b6e3a721ab2
simplon-romainB <yysh@hotmail.fr>
2020-03-04 14:33:24 +0100

    début test e2e

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -1,16 +1,31 @@
 import { AppPage } from './app.po';
-import { browser, logging } from 'protractor';
+import { browser, by, element, logging, ExpectedConditions } from 'protractor';
 
-describe('workspace-project App', () => {
+describe('ome', () => {
   let page: AppPage;
+  let login = element(by.id('connexionEmailInput'));
+  let loginEmail = element(by.id('loginEmail'));
+  let loginPassword = element(by.id('loginPassword'));
+  let loginConnexion = element(by.id('loginConnexion'));
+  let EC = ExpectedConditions;
+
+
+
 
   beforeEach(() => {
-    page = new AppPage();
+    browser.get('/blog');
+
+
   });
 
-  it('should display welcome message', () => {
-    page.navigateTo();
-    expect(page.getTitleText()).toEqual('ome app is running!');
+  it('should login admin', () => {
+    browser.sleep(5000);
+    login.click();
+    loginEmail.sendKeys('romain.barry69@gmail.com');
+    loginPassword.sendKeys('Magicstar198.');
+    loginConnexion.click();
+    browser.wait(EC.elementToBeClickable(element(by.id('addArticleButton'))), 5000);
+    console.log('ok ajouter article est clickable');
   });
 
   afterEach(async () => {
